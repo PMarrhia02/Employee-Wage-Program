ba64422907525eb23d9dcd728552b7633c89c873
Prikshit <mardiyaprikshit@gmail.com>
2025-02-08 10:15:38 +0530

    [Prikshit] Add: Python script to check attendance of an employee

--- emp.py
+++ emp.py
@@ -1,1 +1,12 @@
-print("Welcome To Employee Wage Computation")
+import random
+
+def check_attend():
+    print("Welcome To Employee Wage Computation Prigram on Master Branch")
+    attendance = random.choice([1, 0])
+    if attendance == 1:
+        print("The Employee is present")
+    else:
+        print("The Employee is Absent")
+if __name__ == "__main__":
+  check_attend()
+  print("Welcome to Employee Wage Computation")
