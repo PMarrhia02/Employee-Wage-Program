8afe0acb82c53dfa31582ce873133152a137115c
Prikshit <mardiyaprikshit@gmail.com>
2025-02-08 10:32:31 +0530

    [Prikshit] Add: Python script to check daily wage of an employee

--- emp.py
+++ emp.py
@@ -1,12 +1,11 @@
 import random
+def check_attendance(attendance):
+    if attendance==0:
+        return 0
+    if attendance==1:
+        print("The wage is: ")
+        return 20*8
+atd_random=random.randint(0,1)
+print(check_attendance(atd_random))
 
-def check_attend():
-    print("Welcome To Employee Wage Computation Prigram on Master Branch")
-    attendance = random.choice([1, 0])
-    if attendance == 1:
-        print("The Employee is present")
-    else:
-        print("The Employee is Absent")
-if __name__ == "__main__":
-  check_attend()
-  print("Welcome to Employee Wage Computation")
+
